0faa57cde7b1207de6ce7ed67594ffe9968c759b
RyHarJr <ryharofficial@gmail.com>
2026-04-14 12:00:36 +0700

    Commit Kedua

---
+++ script.js
@@ -0,0 +1,9 @@
+const namaBarang = document.getElementById("namaBarang");
+const hargaBarang = document.getElementById("hargaBarang");
+const stokBarang = document.getElementById("stokBarang");
+
+function tambahBarang() {
+  const nama = namaBarang.value;
+  const harga = hargaBarang.value;
+  const stok = stokBarang.value;
+}
