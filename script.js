725401dcb8e16a04443763228ab8311c856720f8
RyHarJr <ryharofficial@gmail.com>
2026-04-14 12:30:45 +0700

    Commit 4

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ const namaBarang = document.getElementById("nama");
 const jumlahBarang = document.getElementById("jumlah");
 const keteranganBarang = document.getElementById("keterangan");
 
-const localDB = JSON.parse(localStorage.getItem("barang")) || [];
-
 function tambahBarang() {
   const nama = namaBarang.value;
   const jumlah = jumlahBarang.value;
   const keterangan = keteranganBarang.value;
+
+  const localDB = JSON.parse(localStorage.getItem("barang")) || [];
 
   if (!nama || !jumlah || !keterangan) {
     alert("Semua field harus diisi!");
@@ -25,16 +25,17 @@ function tambahBarang() {
   localStorage.setItem("barang", JSON.stringify(localDB));
 
   alert("Barang berhasil ditambahkan!");
+
+  document.getElementById("listBarang").innerHTML = "";
+  tampilkanBarang();
 }
 
 function tampilkanBarang() {
-  const listBarang = document.getElementById("listBarang");
-
-
+  const localDB = JSON.parse(localStorage.getItem("barang")) || [];
 
   localDB.forEach((barang, index) => {
     document.getElementById("listBarang").innerHTML += `
-    <div class="card col-12 col-md-6 col-lg-4">
+    <div class="card col-12 col-md-6 col-lg-4 mb-3">
         <img class="card-img-top" src="https://images.unsplash.com/photo-1573376670774-4427757f7963?q=80&w=1171&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Box" />
         <div class="card-body">
             <h4 class="card-title">${barang.nama}</h4>
@@ -45,3 +46,5 @@ function tampilkanBarang() {
     `;
   });
 }
+
+tampilkanBarang();
